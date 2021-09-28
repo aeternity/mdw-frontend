afb7940522fbd124a1e51fe9846b94eb055974c9
Atanas Krondev <krondev@gmail.com>
2021-09-28 12:08:15 +0300

    fix format token

--- plugins/filters/formatToken.js
+++ plugins/filters/formatToken.js
@@ -1,5 +1,8 @@
 import BigNumber from 'bignumber.js'
 
 export default function (amount, decimals, symbol) {
+  if (!amount) {
+    return `0 ${symbol}`
+  }
   return `${new BigNumber(amount).shiftedBy(-decimals).toString()} ${symbol}`
 }
