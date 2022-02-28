6b7a8516de125a985b88069e24ee702681a3a9f5
badi <badi1wow@gmail.com>
2022-02-28 12:10:13 +0300

    fix: when tokens could not be loaded should display transactions

--- plugins/filters/formatToken.js
+++ plugins/filters/formatToken.js
@@ -1,8 +1,12 @@
 import BigNumber from 'bignumber.js'
 
 export default function (amount, decimals, symbol) {
-  if (!amount) {
-    return `0 ${symbol}`
+  try {
+    if (!amount) {
+      return `0 ${symbol}`
+    }
+    return `${new BigNumber(amount).shiftedBy(-decimals).toString()} ${symbol || ''}`
+  } catch (error) {
+    return null
   }
-  return `${new BigNumber(amount).shiftedBy(-decimals).toString()} ${symbol || ''}`
 }
